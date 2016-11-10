5ba8ca1c4a73b9a76f0de2c9d10ad3c8d844ba79
Patrick Holmes <p_holmes1@u.pacific.edu>
2016-11-10 14:13:23 -0800

    Megaman moves automatically in Tutorial
    Once we have the obstacles built in, we can place triggers where we want
    him to jump.

--- Scripts/MegamanController.cs
+++ Scripts/MegamanController.cs
@@ -10,35 +10,39 @@ public class MegamanController : MonoBehaviour
 	bool onGround = false;
 	public Transform checkForGround;
 	float groundRadius = 0.2f;
-	public LayerMask whatIsGround; //tell character what is considered "ground"
+	public LayerMask whatIsGround; // tell character what is considered "ground"
 	public float jumpForce = 700.0f;
+
 	// Use this for initialization
-	void Start () 
+	void Start() 
 	{
 		animatePlayer = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () 
+	void FixedUpdate() 
 	{
-		onGround = Physics2D.OverlapCircle (checkForGround.position, groundRadius, whatIsGround);//you are checking if it hit anywhere or not
+		// Checking if it hit anywhere or not
+		onGround = Physics2D.OverlapCircle(checkForGround.position, groundRadius, whatIsGround); 
+
 		animatePlayer.SetBool("OnGround", onGround);
-		animatePlayer.SetFloat("vSpeed",GetComponent<Rigidbody2D>().velocity.y);//vertical speed
+		animatePlayer.SetFloat("vSpeed",GetComponent<Rigidbody2D>().velocity.y); // vertical speed
 
 
-		float move = Input.GetAxis("Horizontal");
+		//float move = Input.GetAxis("Horizontal");
 
-		animatePlayer.SetFloat("Speed", Mathf.Abs(move));
+		//animatePlayer.SetFloat("Speed", Mathf.Abs(move));
+		animatePlayer.SetFloat("Speed", Mathf.Abs(maxSpeed));
 
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (/*move **/ maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-		if (move > 0 && !facingRight) {
-			Flip ();
+		/*if (move > 0 && !facingRight) {
+			Flip();
 		} 
 		else if (move < 0 && facingRight) 
 		{
-			Flip ();
-		}
+			Flip();
+		}*/
 
 	}
 
@@ -46,12 +50,12 @@ public class MegamanController : MonoBehaviour
 	{
 		if (onGround && Input.GetKeyDown (KeyCode.Space)) 
 		{
-			animatePlayer.SetBool ("onGround", false);//no longer on ground
-			GetComponent<Rigidbody2D>().AddForce (new Vector2(0, jumpForce));
+			animatePlayer.SetBool ("onGround", false); // no longer on ground
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
 		}
 	}
 
-	void Flip()//flips around character. now we don't have to animate in the other direction
+	void Flip() // flips character around. now we don't have to animate in the other direction
 	{
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
